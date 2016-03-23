fb237e1b8f2a95e8f8b7cbad1f6642c4903bf129
nnaemekaopara <1513552@rgu.ac.uk>
2016-03-23 23:45:56 +0000

    commit

--- submit.php
+++ submit.php
@@ -1,21 +1,24 @@
 <?php
 
-if(!empty($_POST['username']) && !empty($_POST['password']) && !empty($_POST['password']))
+if(!empty($_POST['username']) && !empty($_POST['password'])
+    && !empty($_POST['password'])&& !empty($_POST['phone']))
 {
-    $username = mysqli_real_escape_string($_POST['username']);
-    $email = mysqli_real_escape_string($_POST['email']);
-    $password = md5(mysqli_real_escape_string($_POST['password']));
+    $username = ($_POST['username']);
+    $email = ($_POST['email']);
+    $password = md5($_POST['password']);
+    $phone = ($_POST['phone']);
 
     $sql=mysqli_query("SELECT email FROM users WHERE email='$email'");
+    $result=mysqli_query($db,$sql);
     $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
-    if(mysqli_num_rows($result) > 0)
+    if(mysqli_num_rows($result) == 1)
     {
         $echo = "Sorry...This email already exists...";
     }
     else
     {
         //echo $name." ".$email." ".$password;
-        $registerquery = mysqli_query($db, "INSERT INTO users (username, email, password)VALUES ('$username', '$email', '$password')")or die(mysqli_error($db));
+        $registerquery = mysqli_query($db, "INSERT INTO users (username, email, password, phone)VALUES ('$username', '$email', '$password', '$phone')")or die(mysqli_error($db));
         if($registerquery)
         {
             echo  "<p>Thank You! you are now registered.</p>";
