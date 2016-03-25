5933608fec4aaa0c9a18073e08e41df5af26c41c
nnaemekaopara <1513552@rgu.ac.uk>
2016-03-25 00:22:30 +0000

    commit

--- bugsubmit.php
+++ bugsubmit.php
@@ -3,7 +3,7 @@ session_start();
 include ("connection.php");
 
 if(!empty($_POST['bugtitle']) && !empty($_POST['bugdescription'])
-    && !empty($_POST['dateposted']) &&!empty($_POST['comments']))
+    && !empty($_POST['dateposted']) )
     {
         $bugtitle = mysqli_real_escape_string($db, $_POST['bugtitle']);
         $bugdescription = mysqli_real_escape_string($db, $_POST['bugdescription']);
@@ -23,34 +23,13 @@ if(!empty($_POST['bugtitle']) && !empty($_POST['bugdescription'])
             or die(mysqli_error($db));
             if($registerquery)
             {
-                $comments = mysqli_real_escape_string($db, $_POST['comments']);
-
-                $sql=mysqli_query("SELECT comments FROM comments WHERE comments='$comments'");
-                $result=mysqli_query($db,$sql);
-                if(mysqli_num_rows($result) == 1)
-                {
-                    $echo = "Sorry...This bugtitle already exists...";
-                }
-
-                else
-                {
-                    //echo $name." ".$email." ".$password;
-                    $registerquery = mysqli_query($db, "INSERT INTO comments (comments, dateposted, userID, bugID)VALUES ('$comments', '$dateposted', '$userID', '$bugID')")
-                    or die(mysqli_error($db));
-                    if($registerquery)
-                    {
-                        echo  "<p>\Bug Uploaded</p>";
-                        header("Location:bugloader.php");
-                    }
-                    else
-                    {
-                        echo"<h2>Error</h2>";
-                        echo"<p>Someting's wrong</p>";
-                    }
-
-                }
-
-
+                echo  "<p>\Bug Uploaded</p>";
+                header("Location:bugloader.php");
+            }
+            else
+            {
+                echo"<h2>Error</h2>";
+                echo"<p>Someting's wrong</p>";
             }
 
         }
